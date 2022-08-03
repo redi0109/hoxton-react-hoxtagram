24dd8ddca8c10ee7a088455821d05a231c91a0b3
Demir Boraj <demirboraj97@gmail.com>
2022-08-03 15:48:46 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,27 @@ import "./App.css";
 
 function App() {
   const [images , setImages] = useState([]);
-  const [comments , setComments] = useState([]);
 
   function getImagefromServer(){
-    fetch("http://localhost:3001/images")
+    fetch("http://localhost:3010/images")
     .then(res => res.json())
-    .then(data => 
-      setImages(data.image));
-    }
-  
+    .then (data => 
+      setImages(data));
 
-  function getCommentsFromServer(){
-    fetch("http://localhost:3001/comments")
-    .then(res => res.json())
-    .then(data => 
-      setComments(data.comments));
     }
 
-    useEffect(() => {
+     useEffect(() => {
       getImagefromServer();
-      getCommentsFromServer();
     }, []);
 
-   
-    
-
+  
   return (
     <div className="App">
       <img className="logo" src="assets/hoxtagram-logo.png" />
       <section className="image-container">
         <article className="image-card">
-          <h2 className="title">{}</h2>
+          {images.map(image) => 
+          <h2 className="title"></h2>
           <img src="./assets/image-placeholder.jpg" className="image" />
           <div className="likes-section">
             <span className="likes">0 likes</span>
