dfa23d39ad244f455a9181240b907a11b3d1c098
Demir Boraj <demirboraj97@gmail.com>
2022-08-03 17:32:58 +0200

    format code

--- src/App.tsx
+++ src/App.tsx
@@ -14,57 +14,57 @@ function App() {
     getImagefromServer();
   }, []);
 
-  function cretePost (image, title : string) {
-   fetch("http://localhost:3010/images", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      title,
-      image,
+  function cretePost(image, title: string) {
+    fetch("http://localhost:3010/images", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        image,
+      }),
+    })
+      .then((res) => res.json())
+      .then((newPost) => {
+        setImages([...images, newPost]);
+      });
 
-    }),
-  }).then((res) => res.json())
-  .then((newPost) => {
-    setImages([...images, newPost]);
-});
+    return (
+      <div className="App">
+        <img className="logo" src="assets/hoxtagram-logo.png" />
+        <section className="image-container">
+          <article className="image-card">
+            {images.map((image) => (
+              <>
+                <h2 className="title">{image.title}</h2>
+                <img src={image.image} className="image" />
+                <div className="likes-section">
+                  <span className="likes">{image.likes}</span>
+                  <button className="like-button">♥</button>
+                </div>
+                <ul className="comments">
+                  {image.comments.map((comment) => (
+                    <li>{comment.content}</li>
+                  ))}
+                </ul>
+              </>
+            ))}
 
-  return (
-    <div className="App">
-      <img className="logo" src="assets/hoxtagram-logo.png" />
-      <section className="image-container">
-        <article className="image-card">
-
-          {images.map((image) => (
-            <>
-              <h2 className="title">{image.title}</h2>
-              <img src={image.image} className="image" />
-              <div className="likes-section">
-                <span className="likes">{image.likes}</span>
-                <button className="like-button">♥</button>
-              </div>
-              <ul className="comments">
-              {image.comments.map((comment) =>
-                <li>{comment.content}</li>
-                )}
-              </ul>
-            </>
-          ))}
-
-         
-          <form onSubmit={(event) => {event.preventDefault()
-            cretePost (event.target.image.value, event.target.title.value)}}>
-            <input type="text"name="title" />
-            <input type="text" placeholder="Image URL" />
-            <button type="submit">Post</button>
-          </form>
-
-
-        </article>
-      </section>
-    </div>
-  );
-}
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                cretePost(event.target.image.value, event.target.title.value);
+              }}
+            >
+              <input type="text" name="title" />
+              <input type="text" placeholder="Image URL" />
+              <button type="submit">Post</button>
+            </form>
+          </article>
+        </section>
+      </div>
+    );
+  }
 }
 export default App;
