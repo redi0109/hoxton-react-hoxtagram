aa881a91eddbcbf069a6373bdba09dff7ea91709
Demir Boraj <demirboraj97@gmail.com>
2022-08-03 17:28:31 +0200

    CreatePost

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,28 @@ function App() {
     getImagefromServer();
   }, []);
 
+  function cretePost (image, title : string) {
+   fetch("http://localhost:3010/images", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      image,
+
+    }),
+  }).then((res) => res.json())
+  .then((newPost) => {
+    setImages([...images, newPost]);
+});
+
   return (
     <div className="App">
       <img className="logo" src="assets/hoxtagram-logo.png" />
       <section className="image-container">
         <article className="image-card">
+
           {images.map((image) => (
             <>
               <h2 className="title">{image.title}</h2>
@@ -34,9 +51,20 @@ function App() {
               </ul>
             </>
           ))}
+
+         
+          <form onSubmit={(event) => {event.preventDefault()
+            cretePost (event.target.image.value, event.target.title.value)}}>
+            <input type="text"name="title" />
+            <input type="text" placeholder="Image URL" />
+            <button type="submit">Post</button>
+          </form>
+
+
         </article>
       </section>
     </div>
   );
+}
 }
 export default App;
