17b194286dc6e29e676c3860b5ce1886fd2a91b2
Demir Boraj <demirboraj97@gmail.com>
2022-08-03 16:15:24 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,41 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [images , setImages] = useState([]);
+  const [images, setImages] = useState([]);
 
-  function getImagefromServer(){
+  function getImagefromServer() {
     fetch("http://localhost:3010/images")
-    .then(res => res.json())
-    .then (data => 
-      setImages(data));
+      .then((res) => res.json())
+      .then((data) => setImages(data));
+  }
 
-    }
+  useEffect(() => {
+    getImagefromServer();
+  }, []);
 
-     useEffect(() => {
-      getImagefromServer();
-    }, []);
-
-  
   return (
     <div className="App">
       <img className="logo" src="assets/hoxtagram-logo.png" />
       <section className="image-container">
         <article className="image-card">
-          {images.map(image) => 
-          <h2 className="title"></h2>
-          <img src="./assets/image-placeholder.jpg" className="image" />
-          <div className="likes-section">
-            <span className="likes">0 likes</span>
-            <button className="like-button">♥</button>
-          </div>
-          <ul className="comments">
-            <li>Get rid of these comments</li>
-            <li>And replace them with the real ones</li>
-            <li>From the server</li>
-          </ul>
+          {images.map((image) => (
+            <>
+              <h2 className="title">{image.title}</h2>
+              <img src={image.image} className="image" />
+              <div className="likes-section">
+                <span className="likes">{image.likes}</span>
+                <button className="like-button">♥</button>
+              </div>
+              <ul className="comments">
+              {image.comments.map((comment) =>
+                <li>{comment.content}</li>
+                )}
+              </ul>
+            </>
+          ))}
         </article>
       </section>
     </div>
   );
 }
 export default App;
-
-
